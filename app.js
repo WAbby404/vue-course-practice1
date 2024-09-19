546f6870d3f5b8f801bb7b8516519ac761bded0d
Abby Waddell <wabby.404@gmail.com>
2024-09-19 13:26:38 -0400

    Lots of practice

--- app.js
+++ app.js
@@ -2,12 +2,32 @@ const vm = Vue.createApp({
   data() {
     return {
       firstName: "John",
+      middleName: "",
       lastName: "Doe",
+      url: "https://google.com",
+      age: 20,
     };
   },
   methods: {
+    increment() {
+      console.log("age updated");
+      this.age++;
+    },
+    handleChange(message, event) {
+      event.preventDefault();
+      //   console.log(message);
+      this.lastName = event.target.value;
+    },
+    handleMiddleChange(event) {
+      this.middleName = event.target.value;
+    },
+  },
+  computed: {
     fullName() {
-      return `${this.firstName} ${this.lastName.toUpperCase()}`;
+      console.log("Fullname computed property called.");
+      return `${this.firstName} ${
+        this.middleName
+      } ${this.lastName.toUpperCase()}`;
     },
   },
 }).mount("#app");
